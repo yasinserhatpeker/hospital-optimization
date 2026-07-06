197d2fd38dc274cb38c7e54637a5da1bcfbdead9
Yasin <yasinpeker43@gmail.com>
2026-07-06 16:31:56 +0300

    related import is done for views.py

--- scheduler/views.py
+++ scheduler/views.py
@@ -1,3 +1,6 @@
-from django.shortcuts import render
+from rest_framework.views import APIView
+from rest_framework.response import Response
+from rest_framework import status
+from .serializers import OperationPlanRequestSerializer
+from .services import generate_schedule
 
-# Create your views here.
