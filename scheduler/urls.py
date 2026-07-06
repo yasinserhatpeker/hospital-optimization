062ddee78b5b6aadaa5667ce917fb4984114dce9
Yasin <yasinpeker43@gmail.com>
2026-07-06 17:03:48 +0300

    fix typo

--- scheduler/urls.py
+++ scheduler/urls.py
@@ -2,6 +2,6 @@ from django.urls import path
 from .views import GenerateOperationPlanView
 
 urlpatterns = [
-    path('api/generate-operation-plan/', GenerateOperationPlanView.as_view(), name="generate_plan")
+    path('api/generate-operation-plan', GenerateOperationPlanView.as_view(), name="generate_plan")
 
 ]
